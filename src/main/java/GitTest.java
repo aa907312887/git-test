aea4dd3fcd6ff4843d6e03dd239afc6ad1f38705
aa907312887 <66191263+aa907312887@users.noreply.github.com>
2022-07-23 23:01:18 +0800

    pull commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git222!!");
         System.out.println("hello git4!!");
         System.out.println("push test!!");
+        System.out.println("pull test!!");
     }
 }
